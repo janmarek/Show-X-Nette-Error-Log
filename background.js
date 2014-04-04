abe8c86c1a39d161fde3f53eceb0ed4362bede37
Jan Tvrdík <jan.tvrdik@gmail.com>
2014-04-04 14:18:02 +0200

    fixed compatibility with Tracy

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.webRequest.onCompleted.addListener(
 	function (details) {
 		for (var i = 0; i < details.responseHeaders.length; i++) {
-			if (details.responseHeaders[i].name === 'X-Nette-Error-Log') {
+			var headerName = details.responseHeaders[i].name;
+			if (headerName === 'X-Nette-Error-Log' || headerName === 'X-Tracy-Error-Log') {
 				chrome.tabs.create({
 					url: 'file://' + details.responseHeaders[i].value
 				});
